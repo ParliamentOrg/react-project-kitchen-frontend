834e971dc2e67f5b5fcc04821dec200a05f0a9bc
Galymzhan Aigelov <aigelov@gmail.com>
2021-07-20 18:02:27 +0600

    Article Preview

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 import agent from '../agent';
 import { ARTICLE_FAVORITED, ARTICLE_UNFAVORITED } from '../constants/actionTypes';
 
 const FAVORITED_CLASS = 'btn btn-sm btn-primary';
 const NOT_FAVORITED_CLASS = 'btn btn-sm btn-outline-primary';
 
-const mapDispatchToProps = (dispatch) => ({
-  favorite: (slug) => dispatch({
-    type: ARTICLE_FAVORITED,
-    payload: agent.Articles.favorite(slug),
-  }),
-  unfavorite: (slug) => dispatch({
-    type: ARTICLE_UNFAVORITED,
-    payload: agent.Articles.unfavorite(slug),
-  }),
-});
+const ArticlePreview = ({ article }) => {
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
 
-const ArticlePreview = (props) => {
-  const { article } = props;
   const favoriteButtonClass = article.favorited
     ? FAVORITED_CLASS
     : NOT_FAVORITED_CLASS;
 
   const handleClick = (ev) => {
     ev.preventDefault();
+
     if (article.favorited) {
-      props.unfavorite(article.slug);
+      dispatch({
+        type: ARTICLE_UNFAVORITED,
+        payload: agent.Articles.unfavorite(article.slug),
+      });
     } else {
-      props.favorite(article.slug);
+      dispatch({
+        type: ARTICLE_FAVORITED,
+        payload: agent.Articles.favorite(article.slug),
+      });
     }
   };
 
@@ -61,7 +60,7 @@ const ArticlePreview = (props) => {
       <Link to={`/article/${article.slug}`} className="preview-link">
         <h1>{article.title}</h1>
         <p>{article.description}</p>
-        <span>Read more...</span>
+        <span>{t('Читать далее...')}</span>
         <ul className="tag-list">
           {
             article.tagList.map((tag) => (
@@ -76,4 +75,4 @@ const ArticlePreview = (props) => {
   );
 };
 
-export default connect(() => ({}), mapDispatchToProps)(ArticlePreview);
+export default ArticlePreview;
